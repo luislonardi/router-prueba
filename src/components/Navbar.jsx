a52dbd3e3c8f59aab2bc154fa1a279db011502bc
luislonardi <luislonardi@gmail.com>
2022-03-17 22:16:09 -0300

    17 marzo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+
+function Navbar() {
+    const navLinkStyles= ({isActive})=>{
+        return{
+            fontWeight:isActive ? 'bold':'normal',
+            textDecoration: isActive ? 'none':'underline'
+        }
+
+    }
+  return (
+    <nav>
+        <NavLink to='/' style={navLinkStyles}>Home</NavLink>
+        <NavLink to='/about' style={navLinkStyles}>About</NavLink>
+    </nav>
+  )
+}
+
+export default Navbar
